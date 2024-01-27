2e6b5fb0ca7092eb268f00da11c9f2d8e099bbd2
prabinnnn <prabinbhandari120@gmail.com>
2024-01-27 10:25:17 +0545

    add in

--- in.js
+++ in.js
@@ -1,5 +1,15 @@
+// const middle = (string) => {
+//   const arr = string.slice(1, 3);
+//   return arr;
+// };
+// console.log(middle("rest"));
 const middle = (string) => {
-  const arr = string.shift();
-  return arr.pop();
+  length = string.length;
+  const h =
+    length % 2 === 0
+      ? string.slice(length / 2 - 1, length / 2 + 1)
+      : string[Math.floor(length / 2)];
+  return h;
 };
+
 console.log(middle("rest"));
